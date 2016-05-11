b1e5001df7a4362996ffe4c52f1b159022471df7
HuangChung-Hao重豪 <HuangChung-Hao>
2016-05-12 00:35:27 +0800

    BLE manager on going.

--- app/src/main/java/com/chasewind/cycling/BLEManager.java
+++ app/src/main/java/com/chasewind/cycling/BLEManager.java
@@ -4,14 +4,19 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.content.Intent;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yyerg on 2016/5/4.
@@ -21,24 +26,31 @@ public class BLEManager {
 
     private Activity mActivity;
 
-    private BluetoothManager mbluetoothManager;
+    private BluetoothManager mBluetoothManager;
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothGatt mBluetoothGatt;
     private Integer mConnectionState;
     private final Integer REQUEST_ENABLE_BT = 1;
-    private static final long SCAN_PERIOD = 100000;
+    private static final long SCAN_PERIOD = 10000;
     private boolean mScanning;
-    private ArrayList mScannedBLEDevices = new ArrayList<BluetoothDevice>();
-    private ArrayList mConnectedBLEDevices = new ArrayList<BluetoothDevice>();
+    private ArrayList mScannedDevices;
+    private ArrayList mConnectedDevices;
+
+    private List<BluetoothGattService> mLeServices;
+
     private BluetoothGattService mLeHeartRateService;
 
     private Handler mHandler;
 
     public BLEManager(Activity activity){
         mActivity = activity;
-        this.mbluetoothManager =
+        this.mBluetoothManager =
                 (BluetoothManager) mActivity.getSystemService(mActivity.BLUETOOTH_SERVICE);
-        this.mBluetoothAdapter = mbluetoothManager.getAdapter();
+        this.mBluetoothAdapter = mBluetoothManager.getAdapter();
+
+        mConnectedDevices = new ArrayList<BluetoothDevice>();
+        mScannedDevices = new ArrayList<BluetoothDevice>();
+
         if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             mActivity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
@@ -52,15 +64,14 @@ public class BLEManager {
     }
 
     public void scanBLEDevice(){
-        Log.d("AAAA","!!!!!!!!!!!!!1");
-        mScannedBLEDevices.clear();
-//        mHandler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                mScanning = false;
-//                mBluetoothAdapter.stopLeScan(mBLEScanCallback);
-//            }
-//        }, SCAN_PERIOD);
+        mScannedDevices.clear();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScanning = false;
+                mBluetoothAdapter.stopLeScan(mBLEScanCallback);
+            }
+        }, SCAN_PERIOD);
         mScanning = true;
         mBluetoothAdapter.startLeScan(mBLEScanCallback);
     }
@@ -72,9 +83,105 @@ public class BLEManager {
             @Override
             public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
                 Log.d("BLE", device.toString());
-                if(!mScannedBLEDevices.contains(device)) {
-                    mScannedBLEDevices.add(device);
+                if(!mScannedDevices.contains(device)) {
+                    mScannedDevices.add(device);
                 }
             }
         };
+
+    public boolean connect(BluetoothDevice device) {
+        if (mBluetoothAdapter == null) {
+            Log.d("BLE", "BluetoothAdapter not initialized or unspecified address.");
+            return false;
+        }
+
+        if (device == null) {
+            Log.d("BLE", "Device not found.  Unable to connect.");
+            return false;
+        }
+        // We want to directly connect to the device, so we are setting the autoConnect
+        // parameter to false.
+        mBluetoothGatt = device.connectGatt(mActivity, false, mGattCallback);
+        return true;
+    }
+
+    // connection change and services discovered.
+    private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            String intentAction;
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                intentAction = ACTION_GATT_CONNECTED;
+                mConnectionState = STATE_CONNECTED;
+                broadcastUpdate(intentAction);
+                Log.d("BLE", "Connected to device.");
+                mBluetoothGatt.discoverServices();
+                // Attempts to discover services after successful connection.
+                Log.i("BLE", "Attempting to start service discovery");
+
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                intentAction = ACTION_GATT_DISCONNECTED;
+                mConnectionState = STATE_DISCONNECTED;
+                Log.i("BLE", "Disconnected from GATT server.");
+                broadcastUpdate(intentAction);
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                mLeServices = mBluetoothGatt.getServices();
+                Integer i;
+                for(i=0;i<mLeServices.size();i++){
+                    if(()mLeServices.get(i).getUuid().toString().substring(0,8).equals("0000180d")) {
+                        mLeHeartRateService = mLeServices.get(i);
+                        break;
+                    }
+                }
+                mLeCharacteristic = mLeHeartRateService.getCharacteristics();
+                for(i=0;i<mLeCharacteristic.size();i++){
+                    if(mLeCharacteristic.get(i).getUuid().toString().substring(0,8).equals("00002a37")) {
+                        mLeHeartRateCharacteristic = mLeCharacteristic.get(i);
+                        break;
+                    }
+                }
+
+                Log.d("BLE","***********"+mLeHeartRateCharacteristic.getUuid());
+                List<BluetoothGattDescriptor> mLeDescriptor;
+                mLeDescriptor = mLeHeartRateCharacteristic.getDescriptors();
+
+                for(i=0;i<mLeDescriptor.size();i++){
+                    Log.d("BLE",mLeDescriptor.get(i).getUuid().toString());
+                }
+
+                BluetoothGattDescriptor descriptor = mLeHeartRateCharacteristic.getDescriptor(mLeDescriptor.get(0).getUuid());
+                descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                mBluetoothGatt.writeDescriptor(descriptor);
+                mBluetoothGatt.setCharacteristicNotification(mLeHeartRateCharacteristic, true);
+                if(!mBluetoothGatt.readCharacteristic(mLeHeartRateCharacteristic)){
+                    Log.d("BLE","read");
+                }
+                broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
+            } else {
+                Log.w("BLE", "onServicesDiscovered received: " + status);
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic characteristic,
+                                         int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+            }
+            Log.d("BLE", "onCharacteristicRead");
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt,
+                                            BluetoothGattCharacteristic characteristic) {
+            broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+            Log.d("BLE", "onCharacteristicChanged");
+        }
+    };
 }
